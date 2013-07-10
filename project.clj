69347579f37aa9f87899305e21f77e96e367c72a
Kendall Buchanan <kendall@kendagriff.com>
2013-07-10 14:46:19 -0600

    Updates

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject usps-clj "0.1.0-SNAPSHOT"
-  :description "Interface with USPS's outdated API."
+  :description "Clojure interface to USPS's outdated API."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
