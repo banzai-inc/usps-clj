1a0cd176ce2c6c7f2dd3204a14d5a99ff36795b8
Kendall Buchanan <kendall@kendagriff.com>
2013-07-10 15:14:42 -0600

    Updated url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject usps-clj "0.1.0-SNAPSHOT"
   :description "Clojure interface to USPS's outdated API."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/banzai-inc/usps-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
