31f8484a38208c3dba4770ad0a701efe2bf70c2c
Kendall Buchanan <kendall@kendagriff.com>
2013-08-31 08:32:25 -0600

    move off snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject usps-clj "0.1.0-SNAPSHOT"
+(defproject usps-clj "0.1.0"
   :description "Clojure interface to USPS's outdated API."
   :url "https://github.com/banzai-inc/usps-clj"
   :license {:name "Eclipse Public License"
